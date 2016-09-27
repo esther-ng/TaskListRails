2dc37ef666d8e1f5811c650117ed19b3bff70dd9
Esther Ng <esther.hc.ng@gmail.com>
2016-09-27 16:38:08 -0700

    Working show view that renders new view for each task linked from index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,37 @@
 class TasksController < ApplicationController
-  def index; end
+  def index
+
+    @tasks_list = [
+      "Buy milk",
+      "Go to gym",
+      "Walk dog"
+    ]
+
+    puts "These are the params: #{params}"
+
+  end
+
+  def show
+    @id = params[:id].to_i
+    @tasks_list = [
+      {
+        name:"Buy milk",
+        description: "Go to store and buy milk",
+        completion_status: "Outstanding",
+        completion_date: "TBD"
+      },
+      {
+        name: "Go to gym",
+        description: "Go work out and stop being lazy",
+        completion_status: "Outstanding",
+        completion_date: "TBD"
+      },
+      {
+        name: "Walk dog",
+        description: "The dog is getting fat. Please walk it already.",
+        completion_status: "Complete",
+        completion_date: Date.new
+      }
+    ]
+  end
 end
