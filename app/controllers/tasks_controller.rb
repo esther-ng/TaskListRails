f772e20eb4686b3bd10d8396577bf5de87d1f103
Esther Ng <esther.hc.ng@gmail.com>
2016-09-28 14:21:10 -0700

    Basic functions completed with database

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,44 +1,38 @@
 class TasksController < ApplicationController
   def index
 
-    @tasks_list = [
-      "Buy milk",
-      "Go to gym",
-      "Walk dog",
-      "Clean house"
-    ]
+    @tasks_list = Task.all
 
     puts "These are the params: #{params}"
 
   end
 
   def show
-    @id = params[:id].to_i
-    @tasks_list = [
-      {
-        name:"Buy milk",
-        description: "Go to store and buy milk",
-        completion_status: "Outstanding",
-        completion_date: "TBD"
-      },
-      {
-        name: "Go to gym",
-        description: "Go work out and stop being lazy",
-        completion_status: "Outstanding",
-        completion_date: "TBD"
-      },
-      {
-        name: "Walk dog",
-        description: "The dog is getting fat. Please walk it already.",
-        completion_status: "Complete",
-        completion_date: Time.new
-      },
-      {
-        name: "Clean house",
-        description: "The place is filthy and needs a thorough cleaning",
-        completion_status: "Complete",
-        completion_date: Time.new
-      }
-    ]
+    @task = Task.find(params[:id])
+    #   {
+    #     name:"Buy milk",
+    #     description: "Go to store and buy milk",
+    #     completion_status: "Outstanding",
+    #     completion_date: "TBD"
+    #   },
+    #   {
+    #     name: "Go to gym",
+    #     description: "Go work out and stop being lazy",
+    #     completion_status: "Outstanding",
+    #     completion_date: "TBD"
+    #   },
+    #   {
+    #     name: "Walk dog",
+    #     description: "The dog is getting fat. Please walk it already.",
+    #     completion_status: "Complete",
+    #     completion_date: Time.new
+    #   },
+    #   {
+    #     name: "Clean house",
+    #     description: "The place is filthy and needs a thorough cleaning",
+    #     completion_status: "Complete",
+    #     completion_date: Time.new
+    #   }
+    # ]
   end
 end
