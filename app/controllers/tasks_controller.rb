b6c37ef744dfe1195ebe67a1abe818308a841141
Esther Ng <esther.hc.ng@gmail.com>
2016-09-28 22:06:47 -0700

    Completed tasks#create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,38 +13,16 @@ class TasksController < ApplicationController
     @completed = "Completed" if @task.completed == true
 
     @completed = "Incomplete" if @task.completed == false
-    #   {
-    #     name:"Buy milk",
-    #     description: "Go to store and buy milk",
-    #     completion_status: "Outstanding",
-    #     completion_date: "TBD"
-    #   },
-    #   {
-    #     name: "Go to gym",
-    #     description: "Go work out and stop being lazy",
-    #     completion_status: "Outstanding",
-    #     completion_date: "TBD"
-    #   },
-    #   {
-    #     name: "Walk dog",
-    #     description: "The dog is getting fat. Please walk it already.",
-    #     completion_status: "Complete",
-    #     completion_date: Time.new
-    #   },
-    #   {
-    #     name: "Clean house",
-    #     description: "The place is filthy and needs a thorough cleaning",
-    #     completion_status: "Complete",
-    #     completion_date: Time.new
-    #   }
-    # ]
+
   end
 
   def new
-    
+
   end
 
   def create
+    Task.create(title: params[:title], description: params[:description], completed: params[:completed], completed_at: params[:completed_at])
+
     redirect_to tasks_path
   end
 
