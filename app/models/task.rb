8d4a85d92fad8c505ba1a254c68184a6942a95bb
Esther Ng <esther.hc.ng@gmail.com>
2016-09-30 16:00:16 -0700

    Added titles to pages

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,15 +1,14 @@
 class Task < ActiveRecord::Base
-
   def complete_toggle(id)
-    @task = Task.find(id)
-    if @task.completed
-      @task.completed = false
-      @task.completed_at = nil
+    task = Task.find(id)
+    if task.completed
+      task.completed = false
+      task.completed_at = nil
     else
-      @task.completed = true
-      @task.completed_at = DateTime.now
+      task.completed = true
+      task.completed_at = DateTime.now
     end
 
-    @task.save
+    task.save
   end
 end
